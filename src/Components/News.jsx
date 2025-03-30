08c4485df40ea7be2e67a933a706ea027f329f74
BonyRoy <94554135+BonyRoy@users.noreply.github.com>
2025-03-30 17:35:31 +0530

    ha

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -64,7 +64,7 @@ const News = () => {
   <br/>
       {loading && <LoadingSpinner darkMode={darkMode} />}
       <style>{globalStyles}</style>
-<div style={{padding:"14px"}}>
+<div style={{padding:"15px"}}>
       {renderNewsSection("Top Car News", newsData["car-news"])}
       <br/>
       <br/>
